a5aac13c33e167797173ef8cd2c6c773a7449b67
Jordi Prats <jprats@systemadmin.es>
2015-12-16 14:27:28 +0100

    inicial

---
+++ manifests/cronjob.pp
@@ -0,0 +1,26 @@
+#
+define purgefiles::cronjob(
+                            $path,
+                            $mtime,
+                            $action="-delete",
+                            $type="-type f",
+                            $hour='0',
+                            $minute='0',
+                            $month=undef,
+                            $monthday=undef,
+                            $weekday=undef,
+                            $ensure='present',
+                          ) {
+
+  cron { "cronjob purgefiles ${name}":
+    command  => "find ${path} ${type} -mtime ${mtime} ${action}",
+    user     => 'root',
+    hour     => $hour,
+    minute   => $minute,
+    month    => $month,
+    monthday => $monthday,
+    weekday  => $weekday,
+    ensure   => $ensure,
+  }
+
+}
