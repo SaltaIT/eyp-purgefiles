58b378dbb02515256067a54a108176a44f30c17c
Jordi Prats <jprats@systemadmin.es>
2016-08-17 16:13:36 +0200

    afgit cronjobname

--- manifests/cronjob.pp
+++ manifests/cronjob.pp
@@ -1,20 +1,30 @@
 #
 define purgefiles::cronjob(
-                            $path       = $name,
-                            $action     = "-delete",
-                            $mtime      = undef,
-                            $type       = "f",
-                            $hour       = '0',
-                            $minute     = '0',
-                            $month      = undef,
-                            $monthday   = undef,
-                            $weekday    = undef,
-                            $ensure     = 'present',
-                            $file_iname = undef,
+                            $path        = $name,
+                            $action      = "-delete",
+                            $mtime       = undef,
+                            $type        = "f",
+                            $hour        = '0',
+                            $minute      = '0',
+                            $month       = undef,
+                            $monthday    = undef,
+                            $weekday     = undef,
+                            $ensure      = 'present',
+                            $file_iname  = undef,
+                            $cronjobname = undef,
                           ) {
 
+  if($cronjobname==undef)
+  {
+    $cron_job_name=$cronjobname
+  }
+  else
+  {
+    $cron_job_name="cronjob purgefiles ${name} ${path} ${mtime} ${type} ${hour} ${minute} ${month} ${monthday} ${weekday} ${ensure} ${file_iname}"
+  }
+
   #"find ${path} ${type} -mtime ${mtime} ${action}"
-  cron { "cronjob purgefiles ${name} ${path} ${mtime} ${type} ${hour} ${minute} ${month} ${monthday} ${weekday} ${ensure} ${file_iname}":
+  cron { $cron_job_name:
     ensure   => $ensure,
     command  => inline_template('find <%= @path %> <% if defined?(@file_iname) %>-iname <%= @file_iname %><% end %> <% if defined?(@type) %>-type <%= @type %><% end %> <% if defined?(@mtime) %>-mtime <%= @mtime %><% end %> <%= @action %>'),
     user     => 'root',
