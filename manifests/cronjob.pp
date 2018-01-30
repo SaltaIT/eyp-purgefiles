1cfb3d76d18517a523564c25ca5ae4b1d5f60380
Jordi Prats <jordi.prats@gmail.com>
2018-01-30 13:52:41 +0100

    retry

--- manifests/cronjob.pp
+++ manifests/cronjob.pp
@@ -27,7 +27,7 @@ define purgefiles::cronjob(
   #"find ${path} ${type} -mtime ${mtime} ${action}"
   cron { $cron_job_name:
     ensure   => $ensure,
-    command  => inline_template('find <%= @path %> <% if defined?(@file_iname) %>-iname <%= @file_iname %><% end %> <% if defined?(@type) %>-type <%= @type %><% end %> <% if defined?(@mtime) %>-mtime <%= @mtime %><% end %> <% if @compress %>-exec gzip {} \\;<% else %><%= @action %><% end %>'),
+    command  => inline_template('find <%= @path %> <% if defined?(@file_iname) %>-iname <%= @file_iname %><% end %> <% if defined?(@type) %>-type <%= @type %><% end %> <% if defined?(@mtime) %>-mtime <%= @mtime %><% end %> <% if @compress %>-exec gzip {} \;<% else %><%= @action %><% end %>'),
     user     => 'root',
     hour     => $hour,
     minute   => $minute,
